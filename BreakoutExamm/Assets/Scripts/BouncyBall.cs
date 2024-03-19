b1ba864f20e1b6d10a3dd8052a8f4ef02cbf3700
Sophku13 <sophi.alekseevna@gmail.com>
2024-03-19 13:52:41 +0100

    Victory screen
    You can win the game if there are no more bricks left

--- BreakoutExamm/Assets/Scripts/BouncyBall.cs
+++ BreakoutExamm/Assets/Scripts/BouncyBall.cs
@@ -7,17 +7,23 @@ public class BouncyBall : MonoBehaviour
 {
     public float minY = -5.5f;
     public float maxVelocity = 15f;
+
     private Rigidbody2D rb;
+
     public TextMeshProUGUI scoreTxt;
     public GameObject[] hearts;
+
     public GameObject gameOverScreen;
+    public GameObject gameWonScreen;
     
     private int score = 0;
     private int lives = 5;
+    private int brickCounter;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        brickCounter = FindObjectOfType<LevelGenerator>().transform.childCount;
     }
 
     // Update is called once per frame
@@ -50,6 +56,12 @@ public class BouncyBall : MonoBehaviour
             Debug.Log("Brick destroyed");
             score += 5;
             scoreTxt.text = score.ToString("00000");
+            brickCounter--;
+            if( brickCounter <= 0)
+            {
+                gameWonScreen.SetActive(true);
+                Time.timeScale = 0;
+            }
         }
     }
     void GameOver()
